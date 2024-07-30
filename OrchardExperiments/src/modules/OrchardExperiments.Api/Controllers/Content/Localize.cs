a1d61f23589efad12588c39c33f26f143f2168e7
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-07-30 12:54:25 +0200

    Add content localization support and API endpoint
    Introduced a new Localize.cs controller to handle content localization via API endpoint. Updated relevant dependencies and configurations to support OrchardCore.ContentLocalization. Also, corrected minor issues in Patch.cs and Directory.Packages.props for consistency and functionality.

---
+++ OrchardExperiments/src/modules/OrchardExperiments.Api/Controllers/Content/Localize.cs
@@ -0,0 +1,75 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OrchardCore.ContentLocalization;
+using OrchardCore.ContentLocalization.Models;
+using OrchardCore.ContentManagement;
+using OrchardCore.Contents;
+using OrchardExperiments.Api.Extensions;
+
+namespace OrchardExperiments.Api.Controllers.Content;
+
+[IgnoreAntiforgeryToken, AllowAnonymous]
+[ApiController]
+public class Localize(
+    IContentManager contentManager,
+    IContentLocalizationManager localizationManager,
+    IAuthenticationService authenticationService, 
+    IAuthorizationService authorizationService)
+    : ControllerBase
+{
+    [HttpPost("api/content/{id}/localize")]
+    public async Task<IActionResult> HandleAsync(string id, RequestModel request)
+    {
+        var authenticateResult = await authenticationService.AuthenticateAsync(HttpContext, Schemes.Api);
+        if (authenticateResult.Succeeded) 
+            HttpContext.User = authenticateResult.Principal;
+    
+        if (!await authorizationService.AuthorizeAsync(User, Permissions.RestApiAccess))
+            return this.ChallengeOrForbid(Schemes.Api);
+        
+        var targetCulture = request.CultureCode ?? string.Empty; // Empty is invariant culture.
+        var contentItem = await contentManager.GetAsync(id, VersionOptions.Latest);
+
+        if (contentItem == null)
+            return NotFound();
+
+        if (!await authorizationService.AuthorizeAsync(User, OrchardCore.ContentLocalization.Permissions.LocalizeContent, contentItem))
+            return this.ChallengeOrForbid(Schemes.Api);
+
+        var checkContentItem = await contentManager.NewAsync(contentItem.ContentType);
+
+        // Set the current user as the owner to check for ownership permissions on creation
+        checkContentItem.Owner = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (!await authorizationService.AuthorizeAsync(User, CommonPermissions.EditContent, checkContentItem))
+            return this.ChallengeOrForbid(Schemes.Api);
+
+        var part = contentItem.As<LocalizationPart>();
+
+        if (part == null)
+        {
+            ModelState.AddModelError("", "The specified content item's ContentDefinition requires the LocalizationPart");
+            return BadRequest(ModelState);
+        }
+
+        var alreadyLocalizedContent = await localizationManager.GetContentItemAsync(part.LocalizationSet, targetCulture);
+
+        if (alreadyLocalizedContent != null)
+            return new ObjectResult(alreadyLocalizedContent);
+
+        try
+        {
+            var newContent = await localizationManager.LocalizeAsync(contentItem, targetCulture);
+            return new ObjectResult(newContent);
+        }
+        catch (InvalidOperationException ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+        }
+    }
+
+    public record RequestModel(string? CultureCode);
+}
--- OrchardExperiments/src/modules/OrchardExperiments.Api/Controllers/Content/Patch.cs
+++ OrchardExperiments/src/modules/OrchardExperiments.Api/Controllers/Content/Patch.cs
@@ -7,41 +7,40 @@ using OrchardCore.ContentManagement;
 using OrchardCore.Contents;
 using OrchardExperiments.Api.Extensions;
 
-namespace OrchardExperiments.Api.Controllers.Content
-{
-    [IgnoreAntiforgeryToken, AllowAnonymous]
-    [ApiController]
-    public class Patch(IAuthenticationService authenticationService, IAuthorizationService authorizationService, IContentManager contentManager) : ControllerBase
-    {
-        [HttpPatch("api/content/{id}")]
-        public async Task<IActionResult> HandleAsync(string id, Request request)
-        {
-            var authenticateResult = await authenticationService.AuthenticateAsync(HttpContext, Schemes.Api);
-            if (authenticateResult.Succeeded) 
-                HttpContext.User = authenticateResult.Principal;
-            
-            var contentItem = await contentManager.GetAsync(id, VersionOptions.DraftRequired);
-            
-            if (contentItem == null)
-                return NotFound();
-            
-            if (!await authorizationService.AuthorizeAsync(User, Permissions.RestApiAccess, contentItem))
-                return this.ChallengeOrForbid(Schemes.Api);
-            
-            if (!await authorizationService.AuthorizeAsync(User, CommonPermissions.EditContent, contentItem))
-                return this.ChallengeOrForbid(Schemes.Api);
-            
-            var patch = request.Patch;
-            contentItem.Merge(patch, new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Replace });
-            
-            if (request.Publish)
-                await contentManager.PublishAsync(contentItem);
-            else
-                await contentManager.SaveDraftAsync(contentItem);
+namespace OrchardExperiments.Api.Controllers.Content;
 
-            return new ObjectResult(contentItem);
-        }
+[IgnoreAntiforgeryToken, AllowAnonymous]
+[ApiController]
+public class Patch(IAuthenticationService authenticationService, IAuthorizationService authorizationService, IContentManager contentManager) : ControllerBase
+{
+    [HttpPatch("api/content/{id}")]
+    public async Task<IActionResult> HandleAsync(string id, RequestModel request)
+    {
+        var authenticateResult = await authenticationService.AuthenticateAsync(HttpContext, Schemes.Api);
+        if (authenticateResult.Succeeded) 
+            HttpContext.User = authenticateResult.Principal;
+            
+        if (!await authorizationService.AuthorizeAsync(User, Permissions.RestApiAccess))
+            return this.ChallengeOrForbid(Schemes.Api);
+        
+        var contentItem = await contentManager.GetAsync(id, VersionOptions.DraftRequired);
+            
+        if (contentItem == null)
+            return NotFound();
+            
+        if (!await authorizationService.AuthorizeAsync(User, CommonPermissions.EditContent, contentItem))
+            return this.ChallengeOrForbid(Schemes.Api);
+            
+        var patch = request.Patch;
+        contentItem.Merge(patch, new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Replace });
+            
+        if (request.Publish)
+            await contentManager.PublishAsync(contentItem);
+        else
+            await contentManager.SaveDraftAsync(contentItem);
+
+        return new ObjectResult(contentItem);
     }
     
-    public record Request(JsonNode Patch, bool Publish);
+    public record RequestModel(JsonNode Patch, bool Publish);
 }
